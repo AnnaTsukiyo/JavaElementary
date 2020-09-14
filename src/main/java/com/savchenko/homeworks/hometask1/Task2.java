f263dacdc38d5552bbdbc5f77c96eb816eeaf206
Anna <64918364+AnnaTsukiyo@users.noreply.github.com>
2020-09-14 14:37:48 +0300

    Update Task2.java

--- src/main/java/com/savchenko/homeworks/hometask1/Task2.java
+++ src/main/java/com/savchenko/homeworks/hometask1/Task2.java
@@ -25,7 +25,7 @@ public class Task2 {
 
     private static double triangleArea(double s1, double s2, double s3) {
 
-        double p = (s1 + s2 + s3) / 2.0d;
+        double p = (s1 + s2 + s3) / 2.0;
         double s = Math.sqrt(p * (p - s1) * (p - s2) * (p - s3));
         return s;
     }
