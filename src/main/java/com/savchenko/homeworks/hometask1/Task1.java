4d8ffd13056d94b0a3bad8d158c34241357d46c5
Anna <64918364+AnnaTsukiyo@users.noreply.github.com>
2020-09-14 14:33:03 +0300

    Update Task1.java

--- src/main/java/com/savchenko/homeworks/hometask1/Task1.java
+++ src/main/java/com/savchenko/homeworks/hometask1/Task1.java
@@ -1,12 +1,13 @@
 package com.savchenko.homeworks.hometask1;
 
+import java.util.Scanner;
+
 /**
  * 1.Using following make a program to calculate logarithm of b with the base of a
  * log a (b)  = log10 b / log10 a
  *  Example: a=2, b=8 Result: 3.0
  * */
 
-import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
