fb7a0dc97d425bbbbf4495f92c7dac834bf25763
Anna <64918364+AnnaTsukiyo@users.noreply.github.com>
2020-09-21 08:12:59 +0300

    Update Order.java

--- src/main/java/com/savchenko/homeworks/hometask5/Order.java
+++ src/main/java/com/savchenko/homeworks/hometask5/Order.java
@@ -1,4 +1,64 @@
 package com.savchenko.homeworks.hometask5;
 
+import java.util.Date;
+
 public class Order {
+    private String item;
+    private double size;
+    private int price;
+    private Date deliveryDate;
+
+    public Order(String item, double size, int price, Date deliveryDate) {
+        this.item = item;
+        this.size = size;
+        this.price = price;
+        this.deliveryDate = deliveryDate;
+    }
+
+
+    public String setItem() {
+        return item;
+    }
+
+    public double setSize() {
+        return size;
+    }
+
+    public int setPrice() {
+        return price;
+    }
+
+    public Date setDeliveryDate(String date) {
+        return deliveryDate;
+    }
+
+    public String getItem() {
+
+        return item;
+    }
+
+    public double getSize() {
+
+        return size;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Date getDeliveryDate(Date deliveryDate) {
+
+        return deliveryDate;
+    }
+
+    public String toString() {
+        return "Order {" +
+                "item = '" + item + '\'' +
+                ", size = '" + size + '\'' + " kg" +
+                ", price = '" + price + '\'' + " UAH " +
+                ", date of delivery = '" + deliveryDate + '\'' +
+                '}';
+    }
+
 }
+
