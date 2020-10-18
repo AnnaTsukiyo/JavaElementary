8791a1c193e13c48f03b4d2a4abc48c3598a8c97
AnnaTsukiyo <annett.sav4enko@gmail.com>
2020-10-19 02:52:46 +0300

    Hometask 11 is uploaded

---
+++ src/main/java/com/savchenko/homeworks/hometask11/FileLogger.java
@@ -0,0 +1,25 @@
+package com.savchenko.homeworks.hometask11;
+
+import org.apache.log4j.lf5.LogLevel;
+import java.io.*;
+import static org.apache.log4j.lf5.LogLevel.INFO;
+
+public class FileLogger extends Logger {
+
+    @java.lang.Override
+    public void log(String message) throws IOException {
+        try (Writer fwNew = new FileWriter("My_Loggers.txt",true)) {
+            fwNew.write(getTimeStamp()+ ":" + message + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void log(LogLevel level, String message) {
+        try (Writer fwWriter = new FileWriter("My_loggers_Levels.txt", true)) {
+            fwWriter.write(getTimeStamp() + " " + level + ":" + "\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
