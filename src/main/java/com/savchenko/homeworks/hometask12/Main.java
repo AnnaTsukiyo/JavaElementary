0946fac7a1aada34899e1d6e02b3e96be48cf19f
Anna <64918364+AnnaTsukiyo@users.noreply.github.com>
2020-11-15 16:12:19 +0200

    Update Main.java

--- src/main/java/com/savchenko/homeworks/hometask12/Main.java
+++ src/main/java/com/savchenko/homeworks/hometask12/Main.java
@@ -1,7 +1,6 @@
 package com.savchenko.homeworks.hometask12;
 
 import java.util.HashSet;
-import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * Write the classes "Mouse" and "Cat".
@@ -18,14 +17,16 @@ public class Main {
     public static void main(String[] args) {
 
         Cat cat = new Cat();
-        LinkedBlockingDeque belly = new LinkedBlockingDeque<>(5);
+
         Mouse mouse1 = new Mouse("Jerry", 460);
         Mouse mouse2 = new Mouse("Jane", 245);
-        cat.eat(belly, mouse1);
-        cat.eat(belly, mouse2);
-        HashSet<Mouse> substance = new HashSet<>(belly);
-        substance.addAll(belly);
+        cat.eat(mouse1);
+        cat.eat(mouse2);
+        HashSet<Mouse> substance = new HashSet<>(cat.belly);
+        substance.addAll(cat.belly);
         Mouse key = new Mouse("Jerry", 460);
         System.out.println("Substance contains Jerry or not:" + substance.contains(key));
+
     }
 }
+
