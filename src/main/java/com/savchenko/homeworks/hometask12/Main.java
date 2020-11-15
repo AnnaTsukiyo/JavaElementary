b14cb0ceaf5001585da32d82d4b1a8aeab046d6b
Anna <64918364+AnnaTsukiyo@users.noreply.github.com>
2020-11-15 22:47:34 +0200

    Update Main.java

--- src/main/java/com/savchenko/homeworks/hometask12/Main.java
+++ src/main/java/com/savchenko/homeworks/hometask12/Main.java
@@ -1,32 +1,29 @@
 package com.savchenko.homeworks.hometask12;
 
-import java.util.HashSet;
+import java.util.*;
+import java.util.concurrent.LinkedBlockingDeque;
 
-/**
- * Write the classes "Mouse" and "Cat".
- * Mouse - contains two fields: name and number of kilocalories.
- * The cat will contain a "belly" inside. A cat's belly can contain a maximum of 5 mice.
- * 1) A cat can eat mice. (Eat method). If a cat tries to eat the 6th mouse when it already has 5 in its stomach, it should not work for it.
- * 2) The cat also has a method for removing mice from the body in a natural way, naturally, the most digested mouse is removed from the body first.
- * 3) the cat can also take a laxative and remove all mice from the body at once. They are excreted as a substance containing all mice. those. the method must return this substance.
- * The only thing we want to do with this substance in the client code is to find out as quickly as possible whether there is Jerry's mouse with an energy value of 460 kcal among the victims of the cat.
- */
+public class Cat {
 
-public class Main {
+    LinkedBlockingDeque<Mouse> belly = new LinkedBlockingDeque<>(5);
+    HashSet<Mouse> substance;
 
-    public static void main(String[] args) {
+    void eat(Mouse mouse) {
+        belly.addFirst(mouse);
+        System.out.printf("The mouse %s is added to belly%n", mouse.getName());
+    }
 
-        Cat cat = new Cat();
+    void miceRemovingFromOrganism() {
+        ArrayList<Mouse> mice = new ArrayList<>(belly);
+        Collections.sort(mice);
+        belly.removeFirst();
+        System.out.println("The mouse %s is removed");
+    }
 
-        Mouse mouse1 = new Mouse("Jerry", 460);
-        Mouse mouse2 = new Mouse("Jane", 245);
-        cat.eat(mouse1);
-        cat.eat(mouse2);
-        HashSet<Mouse> substance = new HashSet<>(cat.belly);
-        substance.addAll(cat.belly);
-        Mouse key = new Mouse("Jerry", 460);
-        System.out.println("Substance contains Jerry or not:" + substance.contains(key));
-
+    private HashSet takingLaxative() {
+        substance.addAll(belly);
+        belly.clear();
+        System.out.println("The mice are removed from the belly. Now you can eat a few fresh mice if you are hungry!");
+        return substance;
     }
 }
-
