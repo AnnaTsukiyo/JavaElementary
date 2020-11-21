fe57ed953ad5f7a8f58d30356309688a01ad3f71
Anna <64918364+AnnaTsukiyo@users.noreply.github.com>
2020-11-21 09:54:17 +0200

    Delete DataMapper.java

--- src/main/java/com/savchenko/homeworks/hometask18/hometask18/DataMapper.java
+++
@@ -1,9 +0,0 @@
-package com.savchenko.homeworks.hometask18.hometask18;
-
-public class DataMapper {
-
-    public Person map(String userData) {
-        String[] data = userData.split(",");
-        return new Person(data[0], data[1], Integer.parseInt(data[2]), Integer.parseInt(data[3]), Integer.parseInt(data[4]), data[5]);
-    }
-}
