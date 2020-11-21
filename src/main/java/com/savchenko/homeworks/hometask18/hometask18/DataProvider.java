3f7d1242d52b566f554f59bf0b7d0dfcc7f6da3f
Anna <64918364+AnnaTsukiyo@users.noreply.github.com>
2020-11-21 09:54:25 +0200

    Delete DataProvider.java

--- src/main/java/com/savchenko/homeworks/hometask18/hometask18/DataProvider.java
+++
@@ -1,9 +0,0 @@
-package com.savchenko.homeworks.hometask18.hometask18;
-
-import java.io.FileNotFoundException;
-import java.util.List;
-
-public interface DataProvider {
-
-    List <Person> provide(String fileName) throws FileNotFoundException;
-}
