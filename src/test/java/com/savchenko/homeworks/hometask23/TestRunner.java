ff61678bbabbd8aac99b29f1440e3d39b64734c3
AnnaTsukiyo <annett.sav4enko@gmail.com>
2020-11-25 20:18:08 +0200

    Homework 23 "Writing your JUNIT "is uploaded

---
+++ src/test/java/com/savchenko/homeworks/hometask23/TestRunner.java
@@ -0,0 +1,19 @@
+package com.savchenko.homeworks.hometask23;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class TestRunner {
+    public static void main(String[] args) throws ClassNotFoundException, InvocationTargetException, IllegalAccessException {
+
+        Class cl = Class.forName("com.savchenko.homeworks.hometask23.FractionMyTest");
+
+        Method[] method = cl.getMethods();
+        for (Method md : method) {
+            if (md.isAnnotationPresent(MyTest.class)) {
+                md.invoke(null);
+            }
+        }
+        System.out.println("The end");
+    }
+}
